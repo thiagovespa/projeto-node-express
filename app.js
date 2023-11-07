acbfec10fe0d68d513b8a12a7cf268cb1c43eb23
Thiago Galbiatti Vespa <thiago@thiagovespa.com.br>
2023-11-07 08:41:20 -0300

    fix: correção de mensagem de Olá

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require ('express'); 
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Oi pessoal!');
+    res.send('Oi desenvolvedores!');
 });
 app.listen(3000, function() {
-    console.log('Ouvindo na 3000! Acesse: http://localhost:3000');
+    console.log('Ouvindo na 3000! Branch de Dev. Acesse: http://localhost:3000');
 });
