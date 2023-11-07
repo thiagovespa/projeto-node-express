06ece79d819e23fa50631f9f6ff2cb470785ef0b
Thiago Galbiatti Vespa <thiago@thiagovespa.com.br>
2023-11-07 08:44:09 -0300

    fix: correção de mensagem local

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require ('express'); 
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Oi desenvolvedores!');
+    res.send('Oi dev local!');
 });
 app.listen(3000, function() {
-    console.log('Ouvindo na 3000! Branch de Dev. Acesse: http://localhost:3000');
+    console.log('Ouvindo na 3000! Branch de feature. Acesse: http://localhost:3000');
 });
